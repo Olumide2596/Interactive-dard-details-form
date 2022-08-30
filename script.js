d81dc327478d867fc3ce84c571d1367c448c4643
Olumide2596 <olumide2596@gmail.com>
2022-08-30 22:06:59 +0100

    ...

--- script.js
+++ script.js
@@ -5,22 +5,21 @@ const completed = document.querySelector('.completed');
 const inputFileds = document.querySelectorAll('input');
 
 confirmBtn.addEventListener('click', function () {
-	inputFileds.forEach(input => {
-		if (input.value.trim() === '') {
-			input.nextSibling.remove();
-			input.style.borderColor = 'var(--error-red)';
-			input.insertAdjacentHTML(
-				'afterend',
-				`<p style='color:red; font-size:12px;margin-top:5px;'>Can't be blank</p>`
-			);
+	inputFileds.forEach(
+		input => {
+			if (input.value.trim() === '') {
+				input.nextSibling.remove();
+				input.style.borderColor = 'var(--error-red)';
+				input.insertAdjacentHTML(
+					'afterend',
+					`<p style='color:red; font-size:12px;margin-top:5px;'>Can't be blank</p>`
+				);
+			} else {
+				// form.classList.add('hidden');
+				// completed.classList.remove('hidden');
+				input.nextSibling.textContent = '';
+			}
 		}
-		inputFileds.forEach(input => {
-			if (input.value.trim() === '') return;
-			// form.classList.add('hidden');
-			// completed.classList.remove('hidden');
-			// input.nextSibling.textContent = '';
-		});
-
 		//
-	});
+	);
 });
