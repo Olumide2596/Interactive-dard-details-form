0beddecdfd4bcc0ed4e9b95324da93973a83d5cc
Olumide2596 <olumide2596@gmail.com>
2022-08-30 16:41:57 +0100

    handing functionality

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+const confirmBtn = document.querySelector('.submit');
+const form = document.querySelector('form');
+const completed = document.querySelector('.completed');
+const inputFileds = document.querySelectorAll('input');
+
+confirmBtn.addEventListener('click', function () {
+	inputFileds.forEach(input => {
+		if (input.value.trim() === '') {
+			input.nextSibling.remove();
+			input.style.borderColor = 'var(--error-red)';
+			input.insertAdjacentHTML(
+				'afterend',
+				`<p style='color:red; font-size:12px;margin-top:5px;'>Can't be blank</p>`
+			);
+		} else {
+			form.classList.add('hidden');
+			completed.classList.remove('hidden');
+			input.nextSibling.textContent = '';
+		}
+		//
+	});
+});
