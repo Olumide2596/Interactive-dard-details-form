9c40fe5bb827757274b337382b7019ef2ca733db
Olumide2596 <olumide2596@gmail.com>
2022-08-31 22:13:09 +0100

    COMMIT

--- script.js
+++ script.js
@@ -3,23 +3,53 @@ const confirmBtn = document.querySelector('.submit');
 const form = document.querySelector('form');
 const completed = document.querySelector('.completed');
 const inputFileds = document.querySelectorAll('input');
+const inputNumber = document.querySelector('#number');
+const cardNumber = document.querySelector('.card-number');
+const inputName = document.querySelector('#name');
+const cardName = document.querySelector('.card-name');
+const inputMonth = document.querySelector('#month');
+const inputYear = document.querySelector('#year');
+const cardDate = document.querySelector('.card-date');
+const inputCvc = document.querySelector('#cvc');
+const cardcvc = document.querySelector('.card-cvc');
 
-confirmBtn.addEventListener('click', function () {
-	inputFileds.forEach(
-		input => {
-			if (input.value.trim() === '') {
-				input.nextSibling.remove();
-				input.style.borderColor = 'var(--error-red)';
-				input.insertAdjacentHTML(
-					'afterend',
-					`<p style='color:red; font-size:12px;margin-top:5px;'>Can't be blank</p>`
-				);
-			} else {
-				// form.classList.add('hidden');
-				// completed.classList.remove('hidden');
-				input.nextSibling.textContent = '';
-			}
+const validate = function () {
+	inputFileds.forEach(input => {
+		if (input.value.trim() === '') {
+			input.nextSibling.remove();
+			input.style.borderColor = 'var(--error-red)';
+			input.insertAdjacentHTML(
+				'afterend',
+				`<p style='color:red; font-size:12px;margin-top:5px;'>Can't be blank</p>`
+			);
+		} else {
+			input.nextSibling.textContent = '';
+			input.style.borderColor = 'var(--light-grayish-violet)';
 		}
-		//
-	);
+	});
+};
+
+const cardCreation = function () {
+	inputNumber.value !== ''
+		? (cardNumber.textContent = inputNumber.value)
+		: (cardNumber.textContent = '0000 0000 0000 0000');
+	inputName.value !== ''
+		? (cardName.textContent = inputName.value.toUpperCase())
+		: (cardName.textContent = 'Jane Appleseed');
+	cardDate.textContent = `${inputMonth.value.padStart(
+		2,
+		'0'
+	)}/${inputYear.value.padStart(2, '0')}`;
+	cardcvc.textContent = inputCvc.value.padStart(3, '000');
+};
+
+confirmBtn.addEventListener('click', function (e) {
+	e.preventDefault();
+	validate();
+	cardCreation();
+
+	if ([...inputFileds].every(input => input.value.trim() !== '')) {
+		form.classList.add('hidden');
+		completed.classList.remove('hidden');
+	}
 });
